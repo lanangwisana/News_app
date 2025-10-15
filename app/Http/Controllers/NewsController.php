8cc37e36a5b50bda32aded587139f68e7ee65096
lanangwisana <lanangwisana@gmail.com>
2025-10-15 21:01:23 +0700

    Update NewsController

--- app/Http/Controllers/NewsController.php
+++ app/Http/Controllers/NewsController.php
@@ -9,12 +9,6 @@ class NewsController extends Controller
 {
         public function index()
     {
-        // $response = Http::get('https://gnews.io/api/v4/top-headlines', [
-        //     'token' => config('services.gnews.key'),
-        //     'lang'  => 'en',
-        //     'max'   => 12,
-        // ]);
-
         $response = Http::get('https://newsdata.io/api/1/latest', [
             'apikey' => config('services.newsdata.key'),
             'language'  => 'en',
