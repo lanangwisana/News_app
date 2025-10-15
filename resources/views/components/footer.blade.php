5da21ff94b874b55a24fff8ca32241f2fbd5bfe8
lanangwisana <lanangwisana@gmail.com>
2025-10-15 20:51:24 +0700

    Update footer style

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -4,9 +4,9 @@
             <div class="sm:flex sm:items-center sm:justify-between">
                 <a href="https://flowbite.com/" class="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
                     <img src="https://flowbite.com/docs/images/logo.svg" class="h-8" alt="Flowbite Logo" />
-                    <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
+                    <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Portasl Berita</span>
                 </a>
-                <ul class="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
+                <ul class="flex flex-wrap items-center mb-6 text-sm font-medium text-white sm:mb-0">
                     <li>
                         <a href="#" class="hover:underline me-4 md:me-6">About</a>
                     </li>
@@ -22,7 +22,7 @@
                 </ul>
             </div>
             <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
-            <span class="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="https://flowbite.com/" class="hover:underline">Flowbite™</a>. All Rights Reserved.</span>
+            <span class="block text-sm text-white sm:text-center">© 2023 <a href="https://flowbite.com/" class="hover:underline">Portal Berita™</a>. All Rights Reserved.</span>
         </div>
     </footer>
 </div>
